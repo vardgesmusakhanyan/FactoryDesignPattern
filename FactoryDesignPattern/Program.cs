cf1d1c409517fe9c559dd46332ee044331d4609b
Vardges Musakhanyan <danoville@gmail.com>
2021-12-21 13:39:49 +0400

    Add project files.

---
+++ FactoryDesignPattern/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+
+    interface CarFactory
+    {
+        Car CreateCar(string brand);
+    }
+
+    public class ConcreteCarFactory : CarFactory
+    {
+        public Car CreateCar(string brand)
+        {
+            switch (brand) {
+                case "Mercedes":
+                return new Mercedes();
+                case "BMW":
+                    return new BMW();
+                default:
+                    throw new ArgumentException("invalid brand",brand);
+            }
+        }
+    }
+
+
+
+    public class Car { }
+
+    public class Mercedes : Car { }
+    public class BMW : Car { }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            CarFactory factory = new ConcreteCarFactory();
+
+            Car car = factory.CreateCar(x);
+
+            Console.WriteLine(car.GetType());
+
+            Console.ReadLine();
+        }
+    }
+}
